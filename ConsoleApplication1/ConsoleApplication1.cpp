174dfc53c71438a54d950e1f24dc28c935372cb3
gzt359 <stpxlw@outlook.com>
2022-01-25 21:57:04 +0800

    xix

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -16,7 +16,7 @@ int main()
 	LPCWSTR lpParameter = L" E:\\测试视频.mp4  -noborder -x 1920 -y 1080  -loop 0";
 	STARTUPINFO si{ 0 };
 	PROCESS_INFORMATION pi{ 0 };
-	if (CreateProcess(L"C:\\Program Files\\WindowsApps\\Microsoft.ZuneVideo_10.21111.10511.0_x64__8wekyb3d8bbwe\\Video.UI.exe", (LPWSTR)lpParameter, 0, 0, 0, 0, 0, 0, &si, &pi))
+	if (CreateProcess(L".\\tools\\ffplay.exe", (LPWSTR)lpParameter, 0, 0, 0, 0, 0, 0, &si, &pi))
 	{
 		Sleep(200);
 
